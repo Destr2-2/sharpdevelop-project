02246c048ca956ca52fcab4a5beb9558d537258c
Destr2-2 <modv081214hgtrmca5@cbtis217.edu.mx>
2024-10-24 19:50:39 -0600

    Se agrego comentarios (Version 2)

--- proyecto/proyecto/MainForm.cs
+++ proyecto/proyecto/MainForm.cs
@@ -36,22 +36,24 @@ namespace proyecto
 		
 		
 		
-		
+		// se declara el boton que sera para calcular
 		void BTNcalcularClick(object sender, EventArgs e)
 		{
 			
-			
+			// se agrega la fecha en el orden de anio mes y dia
 			DateTime fecha = DTPfecha.Value;
 			string RFC = fecha.ToString("yyMMdd");
 			
+			// se agrega la letra del nombre para el RFC
 			string nombre = TXTnombre.Text;
 			string prim = nombre.Substring(0,1);
 
-			
+			// lo mismo de arriba pero se agregaran 2 letras
 			string apellido1 = TXTapellido1.Text;
 			string app1 = apellido1.Substring(0, 2);
 
-			
+			// los mismo que lo anterior pero como el nombre solo se agregara la primera letra y
+			// se imprimira en un Leabel el cual al final de este llevara las letras SF6
 			string appellido2 = TXTapellido2.Text;
 			string app2 = appellido2.Substring(0,1);
 			LBLrfc.Text = app1 + app2 + prim + RFC + "SF6";
